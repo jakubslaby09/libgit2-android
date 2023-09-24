b60816f15e0b82cea69f7ff6af980e692d4c5502
Jakub Slabý <jakubslaby09@gmail.com>
2023-09-24 20:15:39 +0200

    my toolchain file

---
+++ android-toolchain.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Android)
+set(CMAKE_SYSTEM_VERSION 34)
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
+set(CMAKE_ANDROID_NDK /opt/android-ndk)
+
+set(OPENSSL_INCLUDE_DIR openssl/include)
+set(OPENSSL_CRYPTO_LIBRARY openssl/libcrypto.so)
+set(OPENSSL_SSL_LIBRARY openssl/libssl.so)
+list(APPEND CMAKE_FIND_ROOT_PATH openssl/include)
+
+add_definitions(-Wno-int-conversion)
+add_compile_options(-Wno-int-conversion)
