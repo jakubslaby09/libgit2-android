b48c9d5d496cd373d418bf47cd782bdc1cf2ae0a
Jakub Slabý <jakubslaby09@gmail.com>
2025-02-21 16:48:34 +0100

    remove config lines that aren't needed anymore

--- android-toolchain.cmake
+++ android-toolchain.cmake
@@ -6,8 +6,4 @@ set(CMAKE_ANDROID_NDK /opt/android-ndk)
 set(OPENSSL_INCLUDE_DIR openssl/include)
 set(OPENSSL_CRYPTO_LIBRARY openssl/libcrypto.so)
 set(OPENSSL_SSL_LIBRARY openssl/libssl.so)
-list(APPEND CMAKE_FIND_ROOT_PATH openssl/include)
 
-add_definitions(-Wno-int-conversion)
-add_compile_options(-Wno-int-conversion)
-add_compile_options(-Wno-implicit-function-declaration)
